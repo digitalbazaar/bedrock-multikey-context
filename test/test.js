11c48dd3ee9699c1497ddc21175432c5edbe725c
David I. Lehn <dlehn@digitalbazaar.com>
2024-08-01 19:59:40 -0400

    Add tests.

---
+++ test/test.js
@@ -0,0 +1,8 @@
+/*!
+ * Copyright (c) 2020-2024 Digital Bazaar, Inc. All rights reserved.
+ */
+import * as bedrock from '@bedrock/core';
+import '@bedrock/multikey-context';
+import '@bedrock/test';
+
+bedrock.start();
